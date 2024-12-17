1798182b34e6e9b1d59c0bdc3f2a59a9be7c6859
eric-zhang <eric_zhang@trendmicro.com>
2024-12-17 11:51:18 -0500

    Create method to get trips by route id

--- app.py
+++ app.py
@@ -12,23 +12,53 @@ load_dotenv()
 def hello():
     print("hello")
 
+def get_vehicle_positions():
+    try:
+        url = "https://nextrip-public-api.azure-api.net/octranspo/gtfs-rt-vp/beta/v1/VehiclePositions?format=json"
+
+        hdr ={
+            # Request headers
+            'Cache-Control': 'no-cache',
+            'Ocp-Apim-Subscription-Key': str(os.getenv('API_KEY')),
+        }
+
+        req = urllib.request.Request(url, headers=hdr)
+
+        req.get_method = lambda: 'GET'
+        response = urllib.request.urlopen(req)
+        print(response.read())
+
+        # parse data
+
+    except Exception as e:
+        print(e)
+
+def get_trips_by_route_id(id: int):
+    try:
+        url = "https://nextrip-public-api.azure-api.net/octranspo/gtfs-rt-tp/beta/v1/TripUpdates?format=json"
+
+        hdr ={
+            # Request headers
+            'Cache-Control': 'no-cache',
+            'Ocp-Apim-Subscription-Key': '2946d77caec44abf9b829e13aaa24595',
+        }
+
+        req = urllib.request.Request(url, headers=hdr)
+
+        req.get_method = lambda: 'GET'
+        response = urllib.request.urlopen(req)
+        # print(response.getcode())
+        # print(json.loads(response.read()))
+        trips = []
+        for obj in json.loads(response.read())['Entity']:
+            if (obj['TripUpdate']['Trip']['RouteId'] == str(id)):
+                trips.append(obj)
+        print(trips)
+        print(len(trips))
+    except Exception as e:
+        print(e)
+
 try:
-    url = "https://nextrip-public-api.azure-api.net/octranspo/gtfs-rt-vp/beta/v1/VehiclePositions?format=json"
-
-    hdr ={
-        # Request headers
-        'Cache-Control': 'no-cache',
-        'Ocp-Apim-Subscription-Key': str(os.getenv('API_KEY')),
-    }
-
-    req = urllib.request.Request(url, headers=hdr)
-
-    req.get_method = lambda: 'GET'
-    response = urllib.request.urlopen(req)
-    print(response.getcode())
-    print(response.read())
-
-    # parse data
-
+    get_entity_by_trip_id(97)
 except Exception as e:
-    print(e)
+    print(f"Exception found: {e}")
