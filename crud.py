28a72b5c20164d2d58d030ff077821b3ba53f786
eric-zhang <eric_zhang@trendmicro.com>
2024-12-18 11:46:32 -0500

    Update mongodb functions

--- crud.py
+++ crud.py
@@ -18,7 +18,7 @@ def delete_route(db, _id):
         print("route does not exist")
 
 
-def add_user_to_route(db, user_id, route_id):
+def add_user_to_route(db, channel, route_id):
     if (db.routes.find_one({"_id": route_id})) is None:
         add_route(db, route_id)
     try:
@@ -26,34 +26,33 @@ def add_user_to_route(db, user_id, route_id):
         data = routes.find()
         for item in data:
             print("get route id", item)
-        routes.update_one({"_id": route_id}, {"$push": {"users": user_id}})
+        routes.update_one({"_id": route_id}, {"$push": {"users": channel}})
 
-        users = db.users
-        if (users.find_one({"_id": user_id})) is None:
-            try:
-                username = generate_random_username()
-                users.insert_one({"_id": user_id, "username": username})
-            except:
-                print("Failed creating user")
+        # users = db.users
+        # if (users.find_one({"_id": user_id})) is None:
+            # try:
+            #     users.insert_one({"_id": user_id, "username": channel})
+            # except:
+            #     print("Failed creating user")
 
     except:
         print("Failed to add user to route")
 
 
-def remove_user_from_route(db, user_id, route_id):
+def remove_user_from_route(db, channel, route_id):
     try:
         routes = db.routes
-        routes.update_one({"_id": route_id}, {"$pull": {"users": user_id}})
+        routes.update_one({"_id": route_id}, {"$pull": {"users": channel}})
     except:
         print("Failed to remove user from route")
 
 
-def remove_user(db, user_id):
-    try:
-        users = db.users
-        users.delete_one({"_id": user_id})
-    except:
-        print("Failed to remove user")
+# def remove_user(db, user_id):
+#     try:
+#         users = db.users
+#         users.delete_one({"_id": user_id})
+#     except:
+#         print("Failed to remove user")
 
 
 def get_users_from_route(db, route_id):
@@ -64,54 +63,34 @@ def get_users_from_route(db, route_id):
         print("Failed to get routes users")
 
 
-def get_route_id_from_user(db, user_id):
-    try:
-        routes = db.routes
-        data = routes.find()
-        for item in data:
-            print("get route id", item)
-        return routes.find_one({"users": {"$in": [user_id]}})["_id"]
-    except:
-        print("Failed to find route user is in")
-        return 0
+# def get_route_id_from_user(db, user_id):
+#     try:
+#         routes = db.routes
+#         data = routes.find()
+#         for item in data:
+#             print("get route id", item)
+#         return routes.find_one({"users": {"$in": [user_id]}})["_id"]
+#     except:
+#         print("Failed to find route user is in")
+#         return 0
 
 
-def get_username(db, user_id):
-    try:
-        users = db.users
-        username = users.find_one({"_id": user_id})["username"]
-        return username
+# def get_username(db, user_id):
+#     try:
+#         users = db.users
+#         username = users.find_one({"_id": user_id})["username"]
+#         return username
 
-    except:
-        print("Failed to retrieve username")
-        return 0
-
-
-def set_username(db, user_id, new_username):
-    try:
-        users = db.users
-        users.update_one({"_id": user_id}, {"$set": {"username": new_username}})
-        return True
-    except:
-        print("Failed to set username")
-        return 0
+#     except:
+#         print("Failed to retrieve username")
+#         return 0
 
 
-def get_random_word():
-    try:
-        with open("./Database/words.json", "r") as file:
-            words_data = json.load(file)
-            words = words_data["data"]
-        return random.choice(words)
-    except Exception as e:
-        print("ERROR:", e)
-        return "---"
-
-
-def generate_random_username():
-    word1 = get_random_word()
-    word2 = get_random_word()
-    word3 = get_random_word()
-
-    random_string = f"{word1.capitalize()}{word2.capitalize()}{word3.capitalize()}"
-    return random_string
+# def set_username(db, user_id, new_username):
+#     try:
+#         users = db.users
+#         users.update_one({"_id": user_id}, {"$set": {"username": new_username}})
+#         return True
+#     except:
+#         print("Failed to set username")
+#         return 0
